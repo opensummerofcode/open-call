fd5367585ecf97b76ccaeb0425620281b7ad96b0
Michiel Leyman <michiel.leyman@gmail.com>
2020-04-01 16:06:12 +0200

    Add Button component

---
+++ src/UI/Button.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let type;
+  export let href;
+  export let primary = false;
+  export let capitalize = false;
+</script>
+
+<style>
+  button {
+    color: var(--color-white);
+    border: 0;
+    padding: 0.8rem 1.6rem;
+    border-radius: 0.4rem;
+    font-weight: bold;
+  }
+
+  .primary {
+    background-color: var(--color-light-blue);
+  }
+
+  .capitalize {
+    text-transform: uppercase;
+  }
+</style>
+
+{#if href}
+  <a {href}>
+    <slot />
+  </a>
+{:else}
+  <button class:primary class:capitalize {type}>
+    <slot />
+  </button>
+{/if}
