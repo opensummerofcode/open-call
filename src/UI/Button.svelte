b40bc1f7c3e855be0c09f6b0aa4052029670c78d
Michiel Leyman <michiel.leyman@gmail.com>
2020-04-02 16:29:24 +0200

    Add outline prop to buttons

--- src/UI/Button.svelte
+++ src/UI/Button.svelte
@@ -3,16 +3,22 @@
   export let href = null;
   export let primary = false;
   export let capitalize = false;
+  export let outline = false;
 </script>
 
 <style>
   button {
     color: var(--color-white);
     border: 0;
-    padding: 1rem 1.6rem;
+    padding: 1.2rem 1.6rem;
     border-radius: 0.4rem;
     font-weight: bold;
     background-color: var(--color-light-blue);
+    cursor: pointer;
+    border: 2px solid transparent;
+    transition: all 300ms;
+    font-size: 1.5rem;
+    text-align: center;
   }
 
   .primary {
@@ -22,14 +28,24 @@
   .capitalize {
     text-transform: uppercase;
   }
+
+  .outline {
+    background-color: transparent;
+    border: 2px solid var(--color-white);
+  }
+
+  .outline:hover {
+    background-color: var(--color-white);
+    color: var(--color-dark-blue);
+  }
 </style>
 
 {#if href}
-  <a {href}>
+  <a {href} class:outline class:primary class:capitalize>
     <slot />
   </a>
 {:else}
-  <button class:primary class:capitalize {type}>
+  <button class:outline class:primary class:capitalize {type}>
     <slot />
   </button>
 {/if}
