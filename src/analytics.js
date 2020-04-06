86fa0376ab6f9f1ab23c8762a3b1b128c6aff5b3
Michiel Leyman <michiel.leyman@gmail.com>
2020-04-06 11:47:43 +0200

    Use analytics IIFE correctly

--- src/analytics.js
+++ src/analytics.js
@@ -1,18 +1,16 @@
-const includeAnalytics = (function (f, a, t, h, o, m) {
-  a[h] =
-    a[h] ||
-    function () {
-      (a[h].q = a[h].q || []).push(arguments);
-    };
-  (o = f.createElement('script')), (m = f.getElementsByTagName('script')[0]);
-  o.async = 1;
-  o.src = t;
-  o.id = 'fathom-script';
-  m.parentNode.insertBefore(o, m);
-})(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
-
 export default () => {
-  includeAnalytics();
+  (function (f, a, t, h, o, m) {
+    a[h] =
+      a[h] ||
+      function () {
+        (a[h].q = a[h].q || []).push(arguments);
+      };
+    (o = f.createElement('script')), (m = f.getElementsByTagName('script')[0]);
+    o.async = 1;
+    o.src = t;
+    o.id = 'fathom-script';
+    m.parentNode.insertBefore(o, m);
+  })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
 
   // eslint-disable-next-line no-undef
   fathom('set', 'siteId', 'WYJVDKXQ');
