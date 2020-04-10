74bfc060c9dc25e15cdf5505daedf924cf606d48
Michiel Leyman <michiel.leyman@gmail.com>
2020-04-10 14:05:10 +0200

    Bundle all vendor css together

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
+import css from 'rollup-plugin-css-only';
 import svg from 'rollup-plugin-svg-import';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
@@ -27,7 +28,7 @@ export default {
         css.write('public/build/bundle.css');
       }
     }),
-
+    css({ output: 'public/build/vendor.css' }),
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
     // some cases you'll need additional configuration -
